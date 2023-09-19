783d935563173dd6277db9ada960740e802e1977
joannakolakowska <kolakowska.joanna.jk@gmail.com>
2023-09-19 21:36:26 +0200

    first problems

--- src/main/java/com/kolakowska/Main.java
+++ src/main/java/com/kolakowska/Main.java
@@ -3,14 +3,33 @@ package com.kolakowska;
 
 import com.kolakowska.Generics.GenericList;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         var list = new GenericList<Integer>();
-        list.add(1);
-        int number = list.get(0);
-        System.out.println(number);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your number: ");
+        int newItem = scanner.nextInt();
+        list.add(newItem);
+
+        System.out.println("Would you like to add another number?");
+        String answer = scanner.nextLine().trim().toLowerCase();
+
+        if (answer.equals("yes")) {
+            System.out.println("hooray");
+        } else if (answer.equals("no")) {
+            System.out.println("What can I get for you?");
+        } else {
+            System.out.println("Please, answer yes or no.");
+            System.out.println("Would you like to add another number?");
+        }
+        int selectedIndex = scanner.nextInt();
+        int number = list.get(selectedIndex);
+        System.out.println("Your number is " + number);
 
         }
 
